f8a5c4e42a1c063ce6abb7e96e52380459f83fe3
nungchucks <102557190+nungchucks@users.noreply.github.com>
2022-11-28 18:16:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const numButtons = document.querySelectorAll(".numButton"); 
+const operands = document.querySelectorAll(".operand");
+const display = document.querySelector('.display');
+const output = document.querySelector('.output');
+const clearButton = document.querySelector('.clearButton');
+const equalsButton = document.querySelector('.equalsButton');
+var num1 = 0, num2 = 0; 
+
+const displayNumber = () => {
+    for (let i = 0; i < numButtons.length; i++) {
+        output.innerHTML = " "; 
+        numButtons[i].addEventListener('click', () => {
+            if (num1) {
+                output.innerHTML = " "; 
+                output.innerHTML += numButtons[i].innerHTML;
+            }
+            else {
+                output.innerHTML+=numButtons[i].innerHTML;
+            }        });
+    }
+}
+displayNumber();
+
+const deleteNumber = () => {
+    clearButton.addEventListener('click', () => {
+        output.innerHTML = output.innerHTML.slice(0, -1);
+    });
+}
+deleteNumber(); 
+
+const clearNumbers = () => {
+    let mouseTimer;
+    function mouseDown() { 
+        mouseUp();
+        mouseTimer = window.setTimeout(execMouseDown,500); //set timeout to fire in 2 seconds when the user presses mouse button down
+    }
+  
+    function mouseUp() { 
+        if (mouseTimer) window.clearTimeout(mouseTimer);  //cancel timer when mouse button is released
+    }
+  
+    function execMouseDown() { 
+        output.innerHTML = " ";
+    }
+
+    clearButton.addEventListener("mousedown", mouseDown);
+    clearButton.addEventListener("mouseup", mouseUp);  //listen for mouse up event on body, not just the element you originally clicked on
+  };
+clearNumbers();
+
+const getNumbers = () => {
+    for (let i = 0; i < operands.length; i++) {
+        operands[i].addEventListener('click', () => {
+            num1=output.innerHTML; 
+            console.log(num1);
+         });
+    }
+}
+
+// const clear = () => {
+//     for (let i = 0; i < numButtons.length; i++) {
+//         numButtons[i].addEventListener('click', () => {
+//             if (num1) {
+//                 output.innerHTML = " "; 
+//                 output.innerHTML += numButtons[i].innerHTML;
+//             }
+//             else {
+//                 output.innerHTML+=numButtons[i].innerHTML;
+//             }
+//         });
+//     }
+// }
+
+
+getNumbers();
+getNumbers(); 
